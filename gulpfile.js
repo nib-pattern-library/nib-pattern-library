2595e97bfe26abfd2d421becea3f69517ba505be
James Newell <j.newell@nib.com.au>
2014-09-17 17:05:06 +1000

    oops forgot to add some files

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var rimraf = require('rimraf');
+var builder = require('nib-component-builder');
+
+gulp.task('clean', function(done) {
+  rimraf('build/', done)
+});
+
+gulp.task('metalsmith', function(done) {
+  require('./metalsmith')(done);
+});
+
+gulp.task('component', function(done) {
+  builder('components', {installDir: __dirname+'/build/components', buildDir:  __dirname+'/build/assets'}, function(errors) {
+    if (errors.length) {
+      done(errors[0]);
+    } else {
+      done();
+    }
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch(['content/*', 'templates/*'], ['metalsmith']);
+  gulp.watch('components/*', ['component']);
+});
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('metalsmith', 'component');
+});
