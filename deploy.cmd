d61dd7f4dcc36a51244e96c8733debfec2e64641
Chris Close <christopher.close@gmail.com>
2014-11-18 23:04:08 +1100

    just try running rimraf...

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,7 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install -g rimraf
-  call :ExecuteCmd !NODE_EXE! rimraf .\node_modules
+  call :ExecuteCmd rimraf .\node_modules
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
