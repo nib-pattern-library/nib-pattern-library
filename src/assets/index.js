390167db55433bd9c77e6fbf38878c4286ea5a57
James Newell <j.newell@nib.com.au>
2015-09-18 12:04:37 +1000

    made off-canvas nice

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,8 @@
-var backToTop = require('@nib/back-to-top')
-require('./lib/navigation')
 
+var offCanvas = require('@nib-components/off-canvas')
+offCanvas({openClass: 'offcanvas--visible'})
+
+var backToTop = require('@nib/back-to-top')
 backToTop({
   element: document.querySelector('.js-back-to-top'),
   hideClass: 'u-hidden',
