232bff3bfeac2f77dd085ee1af8df5ea12e2214c
Laurie Jones <laurie.jones@nib.com.au>
2015-11-24 11:40:43 +1100

    Show more of the forms components

--- src/assets/lib/forms/index.js
+++ src/assets/lib/forms/index.js
@@ -11,24 +11,32 @@ export default class ExampleForm extends React.Component {
 
         <Form title="A React Form" theme="green">
 
-          <Form.Control valid={true} label="What's your name" help="Please enter your first name.">
+          <Form.Control valid validated label="What's your name" help="Please enter your first name.">
             <Form.Text/>
           </Form.Control>
 
-          <Form.Control label="Phone">
+          <Form.Control valid validated label="Phone">
             <Form.Text/>
           </Form.Control>
 
-          <Form.Control label="Email" valid={false} error="Not a valid email.">
+          <Form.Control label="Email" valid={false} validated error="Not a valid email.">
             <Form.Text/>
           </Form.Control>
 
-          <Form.Control valid label="Where do you live?" help="Just the state.">
+          <Form.Control valid={false} validated label="Where do you live?" help="Just the state is fine.">
             <Form.Select options={{nsw: "NSW", vic: "VIC", qld: "QLD", other: "Somewhere else.."}} />
+          </Form.Control>
+
+          <Form.Control valid validated label="Are you male or female?">
+            <Form.RadioGroup name="gender" options={{male: 'Male', female: 'Female'}} defaultValue="female" onChange={event => console.log('change' ,event.target.value)} onBlur={event => console.log('blur', event.target.value)}/>
           </Form.Control>
 
           <Form.Control valid validated label="Can we add you to all of our mailing lists?">
             <Form.Checkbox name="privacy" value="Yes" label="Yes, send me lots of emails please." />
+          </Form.Control>
+
+          <Form.Control valid validated label="What extras do you want?">
+            <Form.CheckboxGroup name="blah" options={{dental: 'Dental', physio: 'Physio', optical: "Optical", chiro: "Chiro", massage: "Massage"}} defaultValue={['dental', 'optical', 'chiro']} />
           </Form.Control>
 
         </Form>
